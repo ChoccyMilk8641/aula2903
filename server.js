b2ef9e859e138a3b82aeaf1e1bb1c5edef4d9bc5
Gabriel Neto <140651218+ChoccyMilk8641@users.noreply.github.com>
2026-03-28 14:15:53 +0000

    nodemon instalado, server pronto

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send ('requisição chegou');
+})
+
+app.get('/home', (req, res) => {
+    res.send ('home');
+})
+
 app.listen(1234, () => {
     console.log("servidor iniciado na porta 1234");
 });
